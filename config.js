bb265c5aa04ad4e36e490112c1cc30be014295e1
6d66jnjsdy-dot <6d66jnjsdy@privaterelay.appleid.com>
2026-09-26 07:05:07 +0300

    Create config.js
    Casino bot configuration

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  PREFIX: "$",
+
+  MIN_BET: 175,
+
+  CURRENCY: "💸",
+
+  COLORS: {
+    WIN: 0x2ecc71,
+    LOSE: 0xe74c3c,
+    INFO: 0x8e44ad,
+    PURPLE: 0x6c3483,
+    NEUTRAL: 0x1c1c1c,
+    ACTIVE: 0xf39c12
+  },
+
+  SECRET_BOARD_USER_ID: "1537816435370229820",
+
+  PORT: Number(process.env.PORT) || 10000
+};
